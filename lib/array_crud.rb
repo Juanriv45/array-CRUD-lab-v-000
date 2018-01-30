d57fde4447be55c177504d1fb6f95af283e63c96
Juan Rivera <juanriv45@gmail.com>
2018-01-30 03:49:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  test = [1,2,3,4]
+  test = [1,2,3,4,5]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -12,7 +12,7 @@ end
 
 def add_element_to_start_of_array(array, element)
 array.unshift("test")
-  
+
 end
 end
 
