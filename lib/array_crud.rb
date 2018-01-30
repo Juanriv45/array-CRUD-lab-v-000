234d069857bccacbc29fbd0335feb2a920ce9ef5
Juan Rivera <juanriv45@gmail.com>
2018-01-30 03:51:09 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  test = [1,2,3,4,5]
+  test = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-array << "test"
+array << "shovel"
 end
 
 def add_element_to_start_of_array(array, element)
